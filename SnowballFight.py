5d3e114b87820ce84bc187e6937ba2d4fbc892ad
alanadurham <alana.durham@d11.org>
2024-12-05 10:57:43 -0700

    saving

--- SnowballFight.py
+++ SnowballFight.py
@@ -1,19 +1,90 @@
 ''' 
     Name: Snowball-Mania
-    Author: 
-    Date: 
+    Author: Alana Durham
+    Date: 12-3-24
     Class: AP Computer Science Principles
-    Python: 
+    Python: 3.11.5
 '''
+
+import random
+import time
 
 def main():
     # the main runner of the game
-	  # welcome the player, gather names, and run the snowball fight!
+	# welcome the player, gather names, and run the snowball fight!
     print("Welcome to Snowball Mania!")
+    name = input("State your name. ")
+    opposition = input("Your presence is a pleasure, " + name + "! Who will you fight? ")
+    print(name + " vs. " + opposition)
+    
+    # number of kills per player
+    killcount = []
+    killcount.append(0)
+    killcount.append(0)
+    
+    players = []
+    players.append(name)
+    players.append(opposition)
+    
+    nextPlayer = ""
+
+    while (nextPlayer != "DONE"):
+        nextPlayer = input("Is that all the players? Type one at a time, or DONE if not. Then, press 'Enter'. ")
+        players.append(nextPlayer)
+        killcount.append(0)
+    players.remove("DONE")
+    
+
+    choice = input("Will you choose who gets hit, or let us decide for you? Type 'yes' or 'no'. ")
+    print(killcount)
+    gameplay(name, players, killcount, choice)
+
+
+
+def gameplay(name, players, killcount, manual):
+    # randomly choose one person to throw the snowball
+    while (len(players) > 1):
+        thrower = random.choice(players)
+        if (thrower == name):
+            if (manual == "yes"):   # manual mode
+                target = input("Your turn, who will you hit? ")
+            else:       # auto mode
+                #print(thrower)
+                target = random.choice(players)
+                while (target == thrower):
+                    target = random.choice(players)
+        else:       # thrower is not the user, picking randomly
+                #print(thrower)
+                target = random.choice(players)
+                while (target == thrower):
+                    target = random.choice(players)
+        #print(target)
+        print(thrower + " threw the snowball at " + target + "!")
+        time.sleep(1)
+        # generate a random number to use as hitNum
+        hitNum = random.randint(1, 5)
+        success = hitResult(hitNum)
+        if (success == True):
+            print("It hits " + target + ", they're down!")
+            # +1 elimination
+            score = players.index(thrower)
+            loser = players.index(target)
+            killcount[score] += 1
+            killcount.pop[loser]
+              
+            players.remove(target)
+        else:
+            print("It misses " + target + ", they live another day...")
+        time.sleep(2)
+    print(players[0] + " wins the battle with " + killcount[0] + " wins!")
+
+
 
 def hitResult(hitNum):
     # based on the number that is passed in, return True or False 
     # indicating if this was a hit or a miss
-    return false
+    if (hitNum == 3): #1 in 5 chance, 3 magic number
+        return True
+    return False
 
 main()
